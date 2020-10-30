9006c0acb828afca5e34ca6a907c8caa16a13934
Patrik Olin <32063174+PatrikOlin@users.noreply.github.com>
2020-10-30 18:49:36 +0100

    Update base64.go

--- cmd/base64.go
+++ cmd/base64.go
@@ -25,13 +25,8 @@ import (
 // base64Cmd represents the base64 command
 var base64Cmd = &cobra.Command{
 	Use:   "base64",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "base64-encoding/decoding",
+	Long: `Fancy and fabulous base64-related utilities.`,
 	Args: func(cmd *cobra.Command, args []string) {
 
 	},
