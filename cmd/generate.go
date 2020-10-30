045dca3e1032005bd450ff5f0afc1a77fc013829
Patrik Olin <32063174+PatrikOlin@users.noreply.github.com>
2020-10-30 19:01:16 +0100

    Update generate.go

--- cmd/generate.go
+++ cmd/generate.go
@@ -32,13 +32,9 @@ var baseUrl = "https://api.fejk.company/v1/"
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Generate fake data",
+	Long: `Generate top tier fake identities and company data. 
+		NOTE: only to be used for mischief.`,
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
 			return errors.New("requires a type argument")
